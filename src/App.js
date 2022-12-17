cfd67aec3fa01c69bf983ed0db54463885ea2a34
Vitor Jaguat <vitorbutkus@gmail.com>
2022-12-17 12:08:45 -0300

    updated nested routes to v6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -11,7 +11,9 @@ function App() {
       <MainHeader />
       <main>
         <Routes>
-          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/" element={<Navigate replace to="/welcome" />} />
+
+          <Route path="/welcome/*" element={<Welcome />} />
 
           <Route path="/products" element={<Products />} />
 
