18b8cb9a725bad96a59712596798ee24668400dd
Vitor Jaguat <vitorbutkus@gmail.com>
2022-12-18 15:06:42 -0300

    initial files v5

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+import NotFound from './pages/NotFound';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <MainHeader />
-      <main>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/welcome" />} />
-
-          <Route path="/welcome/*" element={<Welcome />}>
-            <Route path="new-user" element={<p>Welcome, new user!</p>} />
-          </Route>
-
-          <Route path="/products" element={<Products />} />
-
-          <Route path="/products/:productId" element={<ProductDetail />} />
-        </Routes>
-      </main>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/quotes' />
+        </Route>
+        <Route path='/quotes' exact>
+          <AllQuotes />
+        </Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+        </Route>
+        <Route path='/new-quote'>
+          <NewQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
 export default App;
-
-// our-domain.com/welcome => Welcome Component
-// our-domain.com/products => Products Component
-// our-domain.com/product-detail/a-book
